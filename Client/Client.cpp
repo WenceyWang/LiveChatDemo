52900a965fd793cabb53ca5c932f284c91a687b6
Wencey Wang <wencey@outlook.com>
2017-07-23 04:04:37 +0800

    Add project files.

---
+++ Client/Client.cpp
@@ -0,0 +1,43 @@
+﻿// Client.cpp : main project file.
+
+#include "stdafx.h"
+
+using namespace System;
+
+//I should use FoggyConsole To drow UI But the Time is not enough ┑(￣Д ￣)┍
+
+namespace WenceyWang {
+
+	namespace LiveChatDemo
+	{
+		namespace Client {
+			
+			
+			public ref class App
+			{
+			public:
+
+				App()
+				{
+
+				}
+
+				void Start()
+				{
+
+				}
+
+			};
+		}
+	}
+}
+
+
+int main(array<System::String ^> ^args)
+{
+	WenceyWang::LiveChatDemo::Client::App^ App = gcnew WenceyWang::LiveChatDemo::Client::App();
+
+	App->Start();
+
+	return 0;
+}
